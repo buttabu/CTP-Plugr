3e50dbc6c215253c42ab7e5b5c4d3d196d5cd19f
Akbar Mirza <akbarbmirza@gmail.com>
2017-11-16 19:25:21 -0500

    Add League Rankings header

--- src/components/LeagueTable/LeagueTable.js
+++ src/components/LeagueTable/LeagueTable.js
@@ -10,52 +10,55 @@ export default class LeagueTable extends Component {
   render() {
     return (
       <div className="league-table table-responsive">
-        <table className="table table-striped table-bordered table-hover">
-          <colgroup>
-            <col id="team-name-col"/>
-            <col id="games-played-col"/>
-            <col id="wins-col"/>
-            <col id="ties-col"/>
-            <col id="losses-col"/>
-            <col id="goals-for-against-col"/>
-            <col id="goal-differential-col"/>
-            <col id="points-col"/>
-          </colgroup>
-          <thead>
-            <tr>
-              <th>Team Name</th>
-              <th>Games Played</th>
-              <th>Wins</th>
-              <th>Ties</th>
-              <th>Losses</th>
-              <th>+ / -</th>
-              <th>Goal Differential</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>CCNY Ballers</td>
-              <td>1</td>
-              <td>1</td>
-              <td>0</td>
-              <td>0</td>
-              <td>1.0</td>
-              <td>1.0</td>
-              <td>55</td>
-            </tr>
-            <tr>
-              <td>CCNY Ballers</td>
-              <td>1</td>
-              <td>1</td>
-              <td>0</td>
-              <td>0</td>
-              <td>1.0</td>
-              <td>1.0</td>
-              <td>55</td>
-            </tr>
-          </tbody>
-        </table>
+        <h2>League Rankings</h2>
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered table-hover">
+            <colgroup>
+              <col id="team-name-col"/>
+              <col id="games-played-col"/>
+              <col id="wins-col"/>
+              <col id="ties-col"/>
+              <col id="losses-col"/>
+              <col id="goals-for-against-col"/>
+              <col id="goal-differential-col"/>
+              <col id="points-col"/>
+            </colgroup>
+            <thead>
+              <tr>
+                <th>Team Name</th>
+                <th>Games Played</th>
+                <th>Wins</th>
+                <th>Ties</th>
+                <th>Losses</th>
+                <th>+ / -</th>
+                <th>Goal Differential</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>CCNY Ballers</td>
+                <td>1</td>
+                <td>1</td>
+                <td>0</td>
+                <td>0</td>
+                <td>1.0</td>
+                <td>1.0</td>
+                <td>55</td>
+              </tr>
+              <tr>
+                <td>CCNY Ballers</td>
+                <td>1</td>
+                <td>1</td>
+                <td>0</td>
+                <td>0</td>
+                <td>1.0</td>
+                <td>1.0</td>
+                <td>55</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       
     );
