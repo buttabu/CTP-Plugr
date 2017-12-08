e1b9420f2194384aa51a738972ed0a2ff17ac630
Akbar Mirza <akbarbmirza@gmail.com>
2017-12-07 22:42:42 -0500

    Add Basic Formatting for some of Team Profile Page

--- src/containers/TeamProfile/TeamProfile.js
+++ src/containers/TeamProfile/TeamProfile.js
@@ -13,6 +13,25 @@ class TeamProfile extends Component {
     return (
       <div className="team-profile container">
         <Helmet title="Team Name" />
+        <div className="jumbotron">
+          <h1>Hello, World</h1>
+          <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+          </p>
+          <p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
+        <div className="profile-card">
+          <img src="placehold.it/1280x200" alt="" className="cover-photo"/>
+          <img src="placehold.it/180x180" alt="" className="profile-photo thumbnail"/>
+          <div className="profile-bio">
+            <h1>Team Name</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor elementum malesuada. In sed vehicula dolor. Aenean eu odio turpis. Pellentesque porttitor ultricies luctus. Fusce porta est eu orci facilisis, eget venenatis felis scelerisque. Vestibulum at semper erat, in euismod mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. In non orci lacus.
+            </p>
+          </div>
+        </div>
         <span>Team Profile Here</span>
       </div>
     );
